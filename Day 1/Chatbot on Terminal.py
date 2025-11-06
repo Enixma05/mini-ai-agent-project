c1905b0808aa3d1d55a8b3110ff97199c980d4dc
Fadlullah "Ifad" Hasan <fadlullahhasan5@gmail.com>
2025-11-06 23:34:37 +0800

    tambah fitur simpan riwayat percakapan dan fitur penggenalan user

--- Day 1/Chatbot on Terminal.py
+++ Day 1/Chatbot on Terminal.py
@@ -1,35 +1,47 @@
-from openai import OpenAI
-from dotenv import load_dotenv
+import openai as OpenAI
 import os
+import dotenv as load_dotenv
+import json
 
 load_dotenv()
-
 client = OpenAI(
     api_key=os.getenv("OPENAI_API_KEY")
 )
 
 SYSTEM_PROMPT = "Kamu adalah seorang doktor di bidang Informatika"
+MODEL = "gpt-4o-mini"
 
-history = [
-    {"role": "system", "content": SYSTEM_PROMPT}
-]
+user_name = ""
+
+chat_history = [{
+    "role": "system",
+    "content": SYSTEM_PROMPT
+}]
+
+stream_mode = True
+
+
+def save_chat_history_to_file(filename="riwayat_percakapan.json"):
+    stored_data = chat_history[1:] if len(chat_history) > 1 else []
+
+    with open(filename, "w", encoding="utf-8") as f:
+        json.dump(stored_data, f, indet=2, ensure_ascii=False)
+
+    print(f"\n[System] Riwayat percakapan disimpan ke {filename}\n")
+
 
 while True:
-    user_input = input("Kamu: ")
+    user_input = input(f"{user_name}: ")
 
     if user_input == "/exit":
+        print(f"AI: Sampai jumpa, {user_name}")
         break
 
-    history.append({"role": "user", "content": user_input})
+    if user_input == "/save":
+        save_chat_history_to_file()
+        continue
 
-    response = client.chat.completions.create(
-        model="gpt-4o-mini",
-        messages=history
-    )
-
-    history.append({
-        "role": "assistant",
-        "content": response.choices[0].message.content
+    chat_history.append({
+        "role": "user",
+        "content": user_input
     })
-
-    print(f"AI: {response.choices[0].message.content}")
